93a34a546b44a98d4695ca8e7764557f7632297d
김현우 <hyun0310woo@gmail.com>
2020-11-15 22:10:31 +0900

    Add Skills.js && Skills.css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,12 +7,12 @@ function Skills() {
     return (
         <div className="hero-container">
             <video src="/videos/video-2.mp4" autoPlay loop muted />
-            <h1>asdasd</h1>
-            <p>asdasdasd</p>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle="btn-outline"
                 buttonSize="btn--large">
-                    Get started
+                    GET STARTED
                 </Button>
                 <Button className="btns" buttonStyle="btn--primary"
                 buttonSize="btn--large">
