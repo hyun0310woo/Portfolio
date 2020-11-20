69c4672ac404584113ae4f4b96a3033cc0b7a947
김현우 <hyun0310woo@gmail.com>
2020-11-20 13:56:28 +0900

    changed Img

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,19 +6,14 @@ import "./Skills.css"
 function Skills() {
     return (
         <div className="hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <img className="backgrundImg" src="images/homeImg.jpg" />
+            <div className="profileBox">
+                <img className="profileImg" src="images/profileImg.jpg" />
+            </div>
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                <Button className="btns" buttonStyle="btn-outline"
-                buttonSize="btn--large">
-                    GET STARTED
-                </Button>
-                <Button className="btns" buttonStyle="btn--primary"
-                buttonSize="btn--large">
-                    WATCH TRALILER <i className="far fa-play-circle" />
-                </Button>
-            </div>
+            
+            
         </div>
     )
 }
