69d9b7c8b8fc1b094759a1d2aa88a6e1846e1828
김현우 <hyun0310woo@gmail.com>
2021-01-16 23:37:56 +0900

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
         <nav className="navbar">
             <div className="navbar-container">
               <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                  portfolio  <i className="fab fa-typo3" />
+                  HW
               </Link>
               <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? "fas fa-times" : "fas fa-bars"} />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import "../App.css"
-import Button from "./Button"
 import "./Skills.css"
 
 function Skills() {
     return (
         <div className="hero-container">
-            <img className="backgrundImg" src="images/homeImg.jpg" />
-            <div className="profileBox">
-                <img className="profileImg" src="images/profileImg2.jpg" />
-            </div>
-            <h1>Hello!</h1>
-            <h1>I'm Web Developer</h1>
-            
-            
+            <img className="backgrundImg" src="images/test.jpg" />
+            <h1>FRONT-END DEVELOPER</h1>
         </div>
     )
 }
